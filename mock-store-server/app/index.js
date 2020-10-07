6bbd298b907b8c70ef88af7f91f08811dd42beef
KaliSpass <Kali.spassov@namogoo.com>
2020-10-07 16:59:53 +0300

    Changed aasa

--- mock-store-server/app/index.js
+++ mock-store-server/app/index.js
@@ -5,18 +5,18 @@ const http = require('http'),
 
 
 
-let aasaContent;
-fs.readFile('/app/apple-app-site-association.txt', function (err, data) {
-    if (err) {
-        throw err;
-    }
-    aasaContent = data;
+// let aasaContent;
+// fs.readFile('/app/apple-app-site-association.txt', function (err, data) {
+//     if (err) {
+//         throw err;
+//     }
+//     aasaContent = data;
 //     http.createServer(function(request, response) {
 //         response.setHeader(200, {"Content-Type": "application/json"});
 //         response.write("lol");
 //         response.end();
 //     }).listen(8000);
-});
+// });
 
 // const http = require('http');
 //
@@ -26,7 +26,17 @@ const port = 80;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(aasaContent);
+    res.end("{\n" +
+        "  \"applinks\": {\n" +
+        "    \"apps\": [],\n" +
+        "    \"details\": [\n" +
+        "    {\n" +
+        "      \"appID\": \"5PJ977824G.com.namogoo.CustomerJourneyMockStore\",\n" +
+        "      \"paths\": [\"*\"]\n" +
+        "    }\n" +
+        "    ]\n" +
+        "  }\n" +
+        "}");
 }).listen(process.env.PORT || 5000)
 //
 // server.listen(port, hostname, () => {
