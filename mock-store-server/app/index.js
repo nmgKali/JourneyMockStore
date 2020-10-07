ddf36c30c03f7a04302056776f4dc10bf586fad1
KaliSpass <Kali.spassov@namogoo.com>
2020-10-07 15:33:27 +0300

    More work.

--- mock-store-server/app/index.js
+++ mock-store-server/app/index.js
@@ -4,13 +4,15 @@ const http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
+
+
+// fs.readFile('./index.html', function (err, html) {
+//     if (err) {
+//         throw err;
+//     }
     http.createServer(function(request, response) {
-        response.setHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
+        response.setHeader(200, {"Content-Type": "application/json"});
+        response.write("lol");
         response.end();
     }).listen(8000);
-});
+// });
