482db1a061260ec08b21baf07025335a36829f0f
KaliSpass <Kali.spassov@namogoo.com>
2020-10-07 16:15:16 +0300

    Changes to hello world

--- mock-store-server/app/index.js
+++ mock-store-server/app/index.js
@@ -19,8 +19,8 @@ console.log("Ran index.js")
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'namogoo-test-store.herokuapp.com';
+const port = 80;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
