841e091cad9f59d5e0e744fac22461d5b2a41cbd
KaliSpass <Kali.spassov@namogoo.com>
2020-10-07 15:07:28 +0300

    Index.js added some functionality

---
+++ mock-store-server/index.js
@@ -0,0 +1,16 @@
+console.log("Ran index.js")
+
+const http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.setHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(8000);
+});
