17b582e935b0f22f7278ce003c032a7e4b9394ae
KaliSpass <Kali.spassov@namogoo.com>
2020-10-08 12:52:08 +0300

    Last try before giving up.

--- mock-store-server/app/index.js
+++ mock-store-server/app/index.js
@@ -21,32 +21,42 @@ const http = require('http'),
 
 // const http = require('http');
 //
-const hostname = 'namogoo-test-store.herokuapp.com';
-const port = 80;
+// const hostname = 'namogoo-test-store.herokuapp.com';
+// const port = 80;
+const path = require('path')
+const appDir = path.dirname(require.main.filename);
 //
 const server = http.createServer((req, res) => {
-    // res.statusCode = 200;
-    // if (req.url.indexOf("apple") >= 0) {
-    //     let certificate = openssl('openssl pkcs12 -in Certificates.p12 -clcerts -nokeys -out output_crt.pem');
-    //
-    //
-    //
-    //     res.setHeader('Content-Type', 'application/pkcs7-mime');
-    //     res.end("{\n" +
-    //         "  \"applinks\": {\n" +
-    //         "    \"apps\": [],\n" +
-    //         "    \"details\": [\n" +
-    //         "    {\n" +
-    //         "      \"appID\": \"5PJ977824G.com.namogoo.CustomerJourneyMockStore\",\n" +
-    //         "      \"paths\": [\"*\"]\n" +
-    //         "    }\n" +
-    //         "    ]\n" +
-    //         "  }\n" +
-    //         "}");
-    // } else {
-    //     res.setHeader('Content-Type', 'text/html')
-    //     res.end("<html><head><title>helllllllo</title><meta name='description' content='testing universal links'/></head><body>meow</body></html>")
-    // }
+    res.statusCode = 200;
+    if (req.url.indexOf("apple") >= 0) {
+
+        const aasa = path.join(appDir, 'apple-app-site-association')
+        console.log("joined file from:${appdir}")
+        res.setHeader('Content-Type', 'application/pkcs7-mime')
+        res.end(aasa)
+
+
+
+        // let certificate = openssl('openssl pkcs12 -in Certificates.p12 -clcerts -nokeys -out output_crt.pem');
+        //
+        //
+        //
+        // res.setHeader('Content-Type', 'application/pkcs7-mime');
+        // res.end("{\n" +
+        //     "  \"applinks\": {\n" +
+        //     "    \"apps\": [],\n" +
+        //     "    \"details\": [\n" +
+        //     "    {\n" +
+        //     "      \"appID\": \"5PJ977824G.com.namogoo.CustomerJourneyMockStore\",\n" +
+        //     "      \"paths\": [\"*\"]\n" +
+        //     "    }\n" +
+        //     "    ]\n" +
+        //     "  }\n" +
+        //     "}");
+    } else {
+        res.setHeader('Content-Type', 'text/html')
+        res.end("<html><head><title>helllllllo</title><meta name='description' content='testing universal links'/></head><body>meow</body></html>")
+    }
 
 }).listen(process.env.PORT || 5000)
 //
@@ -55,14 +65,14 @@ const server = http.createServer((req, res) => {
     // console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-const path = require('path')
 
-module.exports = function (root) {
-    return function (req, res) {
-        const aasa = path.join(root, 'apple-app-site-association')
-        console.log("joined file")
-        res.set('Content-Type', 'application/pkcs7-mime')
-        res.status(200)
-        res.sendFile(aasa)
-    }
-}
+
+// module.exports = function (root) {
+//     return function (req, res) {
+//         const aasa = path.join(root, 'apple-app-site-association')
+//         console.log("joined file")
+//         res.set('Content-Type', 'application/pkcs7-mime')
+//         res.status(200)
+//         res.sendFile(aasa)
+//     }
+// }
