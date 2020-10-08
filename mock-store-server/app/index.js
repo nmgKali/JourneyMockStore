136e8a3df6fd3c1d26c2cd4bd637bb98ac7864c4
KaliSpass <Kali.spassov@namogoo.com>
2020-10-08 12:17:14 +0300

    Testing a different solution

--- mock-store-server/app/index.js
+++ mock-store-server/app/index.js
@@ -1,4 +1,5 @@
 console.log("Ran index.js")
+const openssl = require('openssl-nodejs')
 
 const http = require('http'),
     fs = require('fs');
@@ -25,23 +26,27 @@ const port = 80;
 //
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
-    if (req.url.indexOf("apple") >= 0) {
-        res.setHeader('Content-Type', 'application/pkcs7-mime');
-        res.end("{\n" +
-            "  \"applinks\": {\n" +
-            "    \"apps\": [],\n" +
-            "    \"details\": [\n" +
-            "    {\n" +
-            "      \"appID\": \"5PJ977824G.com.namogoo.CustomerJourneyMockStore\",\n" +
-            "      \"paths\": [\"*\"]\n" +
-            "    }\n" +
-            "    ]\n" +
-            "  }\n" +
-            "}");
-    } else {
+    // if (req.url.indexOf("apple") >= 0) {
+    //     let certificate = openssl('openssl pkcs12 -in Certificates.p12 -clcerts -nokeys -out output_crt.pem');
+    //
+    //
+    //
+    //     res.setHeader('Content-Type', 'application/pkcs7-mime');
+    //     res.end("{\n" +
+    //         "  \"applinks\": {\n" +
+    //         "    \"apps\": [],\n" +
+    //         "    \"details\": [\n" +
+    //         "    {\n" +
+    //         "      \"appID\": \"5PJ977824G.com.namogoo.CustomerJourneyMockStore\",\n" +
+    //         "      \"paths\": [\"*\"]\n" +
+    //         "    }\n" +
+    //         "    ]\n" +
+    //         "  }\n" +
+    //         "}");
+    // } else {
         res.setHeader('Content-Type', 'text/html')
         res.end("<html><head><title>helllllllo</title><meta name='description' content='testing universal links'/></head><body>meow</body></html>")
-    }
+    // }
 
 }).listen(process.env.PORT || 5000)
 //
@@ -49,3 +54,15 @@ const server = http.createServer((req, res) => {
 //     console.log("meow");
     // console.log(`Server running at http://${hostname}:${port}/`);
 // });
+
+const path = require('path')
+
+module.exports = function (root) {
+    return function (req, res) {
+        const aasa = path.join(root, 'apple-app-site-association')
+
+        res.set('Content-Type', 'application/pkcs7-mime')
+        res.status(200)
+        res.sendFile(aasa)
+    }
+}
